9f7369e1c8902eb5558f00c17b3e0e5da82a41e0
Uli Preuss <me@ulipreuss.eu>
2012-03-22 14:23:37 +0100

    version 2

--- air.js
+++ air.js
@@ -3,46 +3,59 @@
   var air = {
     
     noscripts: [],
+    display: null,
 
     addImages: function(config, resize, noscripts) {
       
       if(!noscripts) {
         noscripts = air.noscripts;
+      } else {
+        air.noscripts = noscripts;
       }
       var display, res, dot, altattr, source, fileextension, imgpath, img, nos, $nos, parent, hasImg, hasAlt,
-        $noscripts = document.getElementsByTagName('noscript'),
+        img_tag, attrs, attr_parts, attr_name, attr_value, attr_apos, attr_arr = [],
+        $noscripts = document.getElementsByTagName('noscript'), 
         i = 0, length = noscripts.length, iWidth = window.innerWidth || document.body.offsetWidth;
         
-      air.noscripts = noscripts;
-
       for (res in config) {
         if (iWidth >= parseInt(res, 10)) {
           display = config[res];
         }
       }
+      
+      if(air.display === display){
+        return;
+      } else {
+        air.display = display;
+      }
+      
       for (; i < length; i = i + 1) {
         nos = noscripts[i];
         $nos = $noscripts[i];
         
-        hasImg = /src=/.test(nos);
-        if(!hasImg) {
-          return;
-        }
-        source = /src=([^ ]+)/.exec(nos)[1];
-        dot = source.lastIndexOf('.');
-        imgpath = source.substring(1, dot);
-        fileextension = source.substring(dot, source.length - 1);
-
         parent = $nos.parentNode;
         img = document.createElement("img");
-        img.src = imgpath + display + fileextension;
-        
-        hasAlt = /alt=/.test(nos);
-        if(!hasAlt) {
-          altattr = /alt=[\'"]([^\'"]+)/.exec(nos)[1];          
-          img.alt = altattr;
-        }
 
+        img_tag = /<img[^>]+>/.exec(nos)[0];
+        attrs = img_tag.match(/(\w+)=("[^<>"]*"|'[^<>']*'|"\w+)/gi);
+        
+        for(i=0;i<attrs.length;i++) {
+          attr_parts = attrs[i].split('=');
+          attr_name = attr_parts[0].toLowerCase();
+          attr_value = attr_parts[1];
+          attr_apos = attr_value.match(/"|'/);
+          attr_value = attr_value.replace(new RegExp (attr_apos[0], 'g'), '');
+          
+          if(attr_name === 'src') {
+            dot = attr_value.lastIndexOf('.');
+            imgpath = attr_value.substring(0, dot);
+            fileextension = attr_value.substring(dot, attr_value.length);
+            img.src = imgpath + display + fileextension;           
+          } else {
+            img[attr_name] = attr_value;           
+          }
+        }
+                
         if (!resize) {
           
           parent.insertBefore(img, $nos);
